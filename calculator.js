d5f39043735e62875f5d11e904bb43e88d1c9862
Arman Ebtekari <93611871+Armanebtekari@users.noreply.github.com>
2021-12-21 00:05:37 +0330

    Add files via upload

---
+++ calculator.js
@@ -0,0 +1,55 @@
+function onefunction() {
+    calculator.display.value += '1';
+}
+
+function twofunction() {
+    calculator.display.value += '2';
+}
+
+function threefunction() {
+    calculator.display.value += '3';
+}
+
+function fourfunction() {
+    calculator.display.value += '/';
+}
+
+function fivefunction() {
+    calculator.display.value += '4';
+}
+
+function sixfunction() {
+    calculator.display.value += '5';
+}
+
+function sevenfunction() {
+    calculator.display.value += '6';
+}
+
+function eightfunction() {
+    calculator.display.value += '*';
+}
+
+function ninefunction() {
+    calculator.display.value += '7';
+}
+
+function thenfunction() {
+    calculator.display.value += '8';
+}
+
+function elevenfunction() {
+    calculator.display.value += '9';
+}
+
+function twoelvefunctio() {
+    calculator.display.value += '+';
+}
+
+function Thirteenfunction() {
+    calculator.display.value += '0';
+}
+
+function fourteenfunction() {
+    calculator.display.value += '-';
+}
